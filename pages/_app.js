7944328bf75d02e8644f60683ebe37cd02f06b58
YourName <YourEmail@example.com>
2025-10-11 14:35:34 +0900

    favicon

--- pages/_app.js
+++ pages/_app.js
@@ -7,6 +7,8 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>D4rkSt4rs</title>
+        <link rel="icon" href="public/favicon.ico" sizes="64x64"/>
+        <link rel="apple-touch-icon" href="public/d4rkst4rs-64px.png"/>
       </Head>
       <Component {...pageProps} />
     </>
